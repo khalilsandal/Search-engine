15940f3d6e3a0b61a23d4447e1cb0672ee71c996
Khalil SANDAL <khalil.sandal@niji.fr>
2022-10-20 16:49:07 +0100

    init

---
+++ searchEngineBackEnd/Controllers/googleController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using searchEngineBackEnd.Models;
+using System.Net;
+
+namespace searchEngineBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class googleController : ControllerBase
+    {
+        [HttpGet(Name = "Getgoogle")]
+        public IEnumerable<searchEngineBackEnd.Models.Result> Search(string search)
+        {
+            string cx = "d48f883849b6f40b8";
+            string apiKey = "AIzaSyDUS_wOanbDQiawa5slLwb9rIH7RItrUkc";
+            var request = WebRequest.Create("https://cse.google.com/cse?cx=d48f883849b6f40b8&q=" + search);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseString = reader.ReadToEnd();
+            dynamic jsonData = JsonConvert.DeserializeObject(responseString);
+
+            var results = new List<Result>();
+            foreach (var item in jsonData.items)
+            {
+                results.Add(new Result
+                {
+                    Title = item.title,
+                    Link = item.link,
+                    Snippet = item.snippet,
+                });
+            }
+            return results;
+
+
+        }
+    }
+}
---
+++ searchEngineBackEnd/Models/Result.cs
@@ -0,0 +1,12 @@
+﻿namespace searchEngineBackEnd.Models
+{
+    public class Result
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Snippet { get; set; }
+        public string Source { get; set; }
+        public string Query { get; set; }
+        public int Index { get; set; }
+    }
+}
